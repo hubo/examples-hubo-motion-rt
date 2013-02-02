2420214346860984b5fc359f51ee1dcf064ff013
Michael Grey <mxgrey@gatech.edu>
2013-02-02 18:58:18 -0500

    The actual content

---
+++ src/tech-sample.cpp
@@ -0,0 +1,6 @@
+#include "Hubo_Tech.h"
+int main( int argc, char **argv )
+{
+    Hubo_Tech hubo;     // This is the constructor for the hubo object
+    hubo.setJointAngle( RSP, -M_PI/2, true ); // "true" instructs it to send the command right away. If you don't pass an argument here, the function assumes "false".
+}
---
+++ src/tech-trajectory-sample.cpp
@@ -0,0 +1,40 @@
+#include "Hubo_Tech.h"
+#include <vector>
+// Note that "std::vector" is a dynamic array class in C++ (not available in C)
+// This means you can use std::vector to make a variable-sized array of Vector6d
+// Even though std::vector and Vector6d have similar names, they are not directly related to each other
+    
+int main( int argc, char **argv )
+{
+    Hubo_Tech hubo;
+    
+    std::vector<Vector6> angles(5); // This declares "angles" as a dynamic array of Vector6ds with a starting array length of 5
+    angles[0] <<   0.0556916,   0.577126,  0.0816814,  -0.492327, 0, 0;
+    angles[1] <<  -1.07878,  0.408266, -0.477742, -0.665062, 0, 0;
+    angles[2] <<   -1.17367, -0.0540511,  -0.772141,  -0.503859, 0, 0;
+    angles[3] <<  -0.518417,   0.172191,  -0.566084, -0.0727671, 0, 0;
+    angles[4] << 0, 0, 0, 0, 0, 0;
+    // Obviously we could easily set up some code which reads in these values out of a pre-recorded file
+    Vector6d currentPosition;  // This Vector6d will be used to track our current angle configuration
+    
+    double tol = 0.075; // This will be the allowed tolerance before moving to the next point
+    int traj = 0; // This will keep track of our current target on the trajectory
+    while( true )
+    {
+        hubo.update() // This grabs the latest state information
+        
+        hubo.getLeftArmAngleStates( currentPosition ); // This will fill in the values of "currentPosition" by passing it in by reference
+        // If you are unfamiliar with "passing by reference", let me know and I can explain the concept. It's a feature of C++ but not C
+        
+        if( ( currentPosition-angles[traj] ).norm() < tol ) // The class function .norm() is a feature of the Eigen libraries. Eigen has many other extremely useful functions like this.
+        {
+            traj++;
+            if( traj > 4 )
+                traj = 0;
+        }
+        
+        hubo.setLeftArmAngles( angles[traj] ); // Notice that the second argument is not passed in, making it default to "false"
+        
+        hubo.sendControls(); // This will send off all the latest control commands over ACH
+    }
+}
