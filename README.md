ccd86f5e58731c38a12f8b2ad08233e0c7aadd60
Michael Grey <mxgrey@gatech.edu>
2013-02-04 04:36:07 -0500

    Changed some documentation

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-examples-Hubo_Tech
-==================
+examples-Hubo-Motion-RT
+=======================
 
-These are source code examples for executables which utilize the Hubo_Tech library/architecture.
+These are source code examples for executables which utilize the Hubo_Tech library to interface
+with the Hubo-Motion-RT package.
--- src/tech-sample.cpp
+++ src/tech-sample.cpp
@@ -2,5 +2,5 @@
 int main( int argc, char **argv )
 {
     Hubo_Tech hubo;     // This is the constructor for the hubo object
-    hubo.setJointAngle( RWY, -M_PI/2, true ); // "true" instructs it to send the command right away. If you don't pass an argument here, the function assumes "false".
+    hubo.setJointAngle( REB, -M_PI/2, true ); // "true" instructs it to send the command right away. If you don't pass an argument here, the function assumes "false".
 }
